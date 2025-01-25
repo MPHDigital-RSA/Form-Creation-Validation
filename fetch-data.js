5aeb581203c5c49321fa8904807f099cdb57593b
Prince Hlela <mphdigital@protonmail.com>
2025-01-25 13:24:39 +0200

    fetching data from API done

---
+++ fetch-data.js
@@ -0,0 +1,37 @@
+
+
+async function fetchUserData () {
+
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    const dataContainer = document.getElementById('api-data');
+
+    try{
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+
+        dataContainer.innerHTML = '';
+
+        let userList = document.createElement('ul');
+
+        users.forEach(user => {
+            console.log(user.username);
+
+            const li = document.createElement('li');
+
+            li.textContent = user.username;
+
+            userList.append(li);
+        });
+
+        dataContainer.append(userList);
+    }catch(error){
+        dataContainer.innerHTML = '';
+        dataContainer.textContent = 'Failed to load user data.';
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchUserData();
+})
